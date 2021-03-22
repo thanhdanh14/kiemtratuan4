8615de67677dd6a7203bba0eee358a54c2222beb
bien thanh danh <thanhdanh010499@gmail.com>
2021-03-22 10:07:11 +0700

    txtUsername

---
+++ login.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+	<title>Trang đăng nhập</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<form method="POST" action="login.php">
+		<fieldset>
+			<legend>Đăng nhập</legend>
+			<tr>
+				<td>Username</td>
+				<td><input type="text" name="username" size="30"></td>
+			</tr>
+		</fieldset>
+	</form>
+</body>
+
+</html>
